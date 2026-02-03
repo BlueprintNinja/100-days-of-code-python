e27dd672191a7d071912325a8090e73c0a3d8d9d
Ray Skinner <125406683+BlueprintNinja@users.noreply.github.com>
2026-02-03 13:32:39 -0600

    echo-logic.py

---
+++ echo-logic.py
@@ -0,0 +1,8 @@
+# Greeting
+print("Welcome to EchoLogic, the Band Name Generator!")
+# Ask user for city
+city = input("What city did you grow up in?\n")
+# Ask user for pet
+pet = input("What was your favorite pet?\n")
+# Tell the user their band name
+print("Your band name is: " + city + " " + pet)
